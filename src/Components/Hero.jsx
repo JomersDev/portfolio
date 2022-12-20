1024d55827af5f20f35b46eeffd6727489318cb7
JomersDev <james.mcgrory135@gmail.com>
2022-12-20 13:47:20 +1000

    page structure

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Hero() {
+    return (
+        <section className="intro" id="home">
+            <h1 className="section__title section__title--intro">
+                Hi, I am <strong>James McGrory</strong>
+            </h1>
+            <p className="section__subtitle section__subtitle--intro">front-end dev</p>
+            <img src="./src/img/dev-jane-01.jpg" alt="a picture of Jane Smith smiling" />
+        </section>
+    )
+}
---
+++ src/Components/Work.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default function Work() {
+    return (
+        <section className="my-work" id="work">
+            <h2 className="section__title">My Work</h2>
+            <p className="section__subtitle"></p>
+            <div className="portfolio">
+                <a href="#" className="portfolio__item">
+                    <img src="./src/img/portfolio-01.jpg" alt="" className="portfolio__img" />
+                </a>
+                
+                <a href="#" className="portfolio__item">
+                    <img src="./src/img/portfolio-02.jpg" alt="" className="portfolio__img" />
+                </a>
+                
+                <a href="#" className="portfolio__item">
+                    <img src="./src/img/portfolio-03.jpg" alt="" className="portfolio__img" />
+                </a>
+                
+                <a href="#" className="portfolio__item">
+                    <img src="./src/img/portfolio-04.jpg" alt="" className="portfolio__img" />
+                </a>
+                
+                <a href="#" className="portfolio__item">
+                    <img src="./src/img/portfolio-05.jpg" alt="" className="portfolio__img" />
+                </a>
+                
+                <a href="#" className="portfolio__item">
+                    <img src="./src/img/portfolio-06.jpg" alt="" className="portfolio__img" />
+                </a>
+                
+                <a href="#" className="portfolio__item">
+                    <img src="./src/img/portfolio-07.jpg" alt="" className="portfolio__img" />
+                </a>
+
+                <a href="#" className="portfolio__item">
+                    <img src="./src/img/portfolio-08.jpg" alt="" className="portfolio__img" />
+                </a>
+
+                <a href="#" className="portfolio__item">
+                    <img src="./src/img/portfolio-09.jpg" alt="" className="portfolio__img" />
+                </a>
+
+                <a href="#" className="portfolio__item">
+                    <img src="./src/img/portfolio-10.jpg" alt="" className="portfolio__img" />
+                </a>
+            </div>
+        </section>
+    )
+}
